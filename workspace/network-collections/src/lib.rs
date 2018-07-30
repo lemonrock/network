453b7425c1f7c05c31cfacec591b0efa544d9acc
Raphael Cohn <raphael.cohn@stormmq.com>
2018-07-30 09:46:18 +0100

    Various IntelliJ related corrections and changes to support Mac OS X

--- workspace/network-collections/src/lib.rs
+++ workspace/network-collections/src/lib.rs
@@ -18,7 +18,7 @@
 extern crate dpdk_unix;
 extern crate libc;
 #[macro_use] extern crate likely;
-extern crate tcp_engine_time;
+extern crate network_time;
 
 
 /// An efficient arena allocator.
@@ -30,58 +30,24 @@ pub mod least_recently_used_cache;
 
 
 /// Magic ring buffers.
-pub mod magic_ring_buffer;
+#[cfg(any(os = "android", os = "linux"))] pub mod magic_ring_buffer;
 
 
-use ::dpdk_unix::page_size;
-use ::dpdk_unix::android_linux::page_table::HasVirtualAddress;
-use ::dpdk_unix::memory_information::PhysicalAddress;
-use ::dpdk_unix::memory_information::VirtualAddress;
-use ::libc::c_void;
-use ::libc::close;
-use ::libc::ftruncate;
-use ::libc::mkstemps;
-use ::libc::mlock;
-use ::libc::mmap;
-use ::libc::PROT_NONE;
-use ::libc::PROT_READ;
-use ::libc::PROT_WRITE;
-use ::libc::MAP_ANONYMOUS;
-use ::libc::MAP_FAILED;
-use ::libc::MAP_FIXED;
-use ::libc::MAP_NORESERVE;
-use ::libc::MAP_PRIVATE;
-use ::libc::MAP_SHARED;
-use ::libc::munmap;
-use ::libc::unlink;
-use ::std::cell::RefCell;
 use ::std::collections::HashMap;
 use ::std::cmp::Eq;
-use ::std::ffi::CString;
 use ::std::fmt;
 use ::std::fmt::Debug;
-use ::std::fmt::Display;
 use ::std::fmt::Formatter;
 use ::std::hash::Hash;
 use ::std::hash::Hasher;
-use ::std::io;
 use ::std::mem::align_of;
 use ::std::mem::ManuallyDrop;
 use ::std::mem::size_of;
-use ::std::ops::Add;
-use ::std::ops::AddAssign;
-use ::std::ops::Mul;
-use ::std::ops::Sub;
-use ::std::ops::SubAssign;
 #[allow(unused_imports)] use ::std::os::unix::ffi::OsStrExt;
-use ::std::path::Path;
 use ::std::ptr::NonNull;
 use ::std::ptr::null_mut;
-use ::std::rc::Rc;
-use ::std::slice::from_raw_parts;
-use ::std::slice::from_raw_parts_mut;
-use ::tcp_engine_time::MillisecondDuration;
-use ::tcp_engine_time::MonotonicMillisecondTimestamp;
+use ::network_time::MillisecondDuration;
+use ::network_time::MonotonicMillisecondTimestamp;
 
 
 include!("BoundedHashMap.rs");
--- workspace/network-collections/src/magic_ring_buffer/mod.rs
+++ workspace/network-collections/src/magic_ring_buffer/mod.rs
@@ -3,6 +3,40 @@
 
 
 use super::*;
+use ::dpdk_unix::page_size;
+use ::dpdk_unix::android_linux::page_table::HasVirtualAddress;
+use ::dpdk_unix::memory_information::PhysicalAddress;
+use ::dpdk_unix::memory_information::VirtualAddress;
+use ::libc::c_void;
+use ::libc::close;
+use ::libc::ftruncate;
+use ::libc::mkstemps;
+use ::libc::mlock;
+use ::libc::mmap;
+use ::libc::PROT_NONE;
+use ::libc::PROT_READ;
+use ::libc::PROT_WRITE;
+use ::libc::MAP_ANONYMOUS;
+use ::libc::MAP_FAILED;
+use ::libc::MAP_FIXED;
+use ::libc::MAP_NORESERVE;
+use ::libc::MAP_PRIVATE;
+use ::libc::MAP_SHARED;
+use ::libc::munmap;
+use ::libc::unlink;
+use ::std::cell::RefCell;
+use ::std::ffi::CString;
+use ::std::fmt::Display;
+use ::std::io;
+use ::std::ops::Add;
+use ::std::ops::AddAssign;
+use ::std::ops::Mul;
+use ::std::ops::Sub;
+use ::std::ops::SubAssign;
+use ::std::path::Path;
+use ::std::rc::Rc;
+use ::std::slice::from_raw_parts;
+use ::std::slice::from_raw_parts_mut;
 
 
 include!("Bytes.rs");
